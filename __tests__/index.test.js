b25e5fc15b5554a39a1257ce8d69c24bacc2b009
Igor Nazim <igor.mainevent@gmail.com>
2022-08-03 09:44:26 +0300

    Пятый шаг

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,12 +3,11 @@ import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import genDiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const expectedUnswer = fs.readFileSync(getFixturePath('result_plain.txt'), 'utf-8');
-
 test('genDiff', () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  const expectedUnswer = fs.readFileSync(getFixturePath('result_plain.txt'), 'utf-8');
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expectedUnswer);
   expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(expectedUnswer);
 });
--- src/index.js
+++ src/index.js
@@ -8,15 +8,11 @@ const genDiff = (firstFilePath, secondFilePath) => {
   const firstFileContent = fs.readFileSync(getAbsFirstFilePath, 'utf-8');
   const getAbsSecondFilePath = path.resolve(process.cwd(), secondFilePath);
   const secondFileContent = fs.readFileSync(getAbsSecondFilePath, 'utf-8');
-  const extName1 = path.extname(firstFilePath);
-  const extName2 = path.extname(secondFilePath);
-  const type1 = (extName1 === '.json') ? 'json' : 'yml';
-  const type2 = (extName2 === '.json') ? 'json' : 'yml';
+  const type1 = (path.extname(firstFilePath) === '.json') ? 'json' : 'yml';
+  const type2 = (path.extname(secondFilePath) === '.json') ? 'json' : 'yml';
   const firstParsedFile = runParser(firstFileContent, type1);
   const secondParsedFile = runParser(secondFileContent, type2);
-  const keys1 = Object.keys(firstParsedFile);
-  const keys2 = Object.keys(secondParsedFile);
-  const unionKeys = _.union(keys1, keys2).sort();
+  const unionKeys = _.union(Object.keys(firstParsedFile), Object.keys(secondParsedFile)).sort();
   const diff = unionKeys.reduce((acc, key) => {
     const check1 = Object.hasOwn(firstParsedFile, key);
     const check2 = Object.hasOwn(secondParsedFile, key);
