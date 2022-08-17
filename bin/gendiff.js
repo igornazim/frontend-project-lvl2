b7f17edd4a26ef397fcf52e69b29d0c94b12312e
Igor Nazim <igor.mainevent@gmail.com>
2022-08-17 14:13:29 +0300

    Шестой шаг завершен

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,6 +11,8 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2, program.opts().format)));
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
+  });
 
 program.parse();
